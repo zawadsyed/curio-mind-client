31d6542d1c0b55b5e5ec03d73e7a3db3b277721e
Syed Zawad <syedzawadd@gmail.com>
2022-10-26 22:29:02 +0600

    added environment variabale

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAPWIc9lAj2NAhvtCGsehdrxVhGpxoKHC4",
-    authDomain: "curio-mind.firebaseapp.com",
-    projectId: "curio-mind",
-    storageBucket: "curio-mind.appspot.com",
-    messagingSenderId: "1022765878427",
-    appId: "1:1022765878427:web:a17b29589be0e8d16e0a3e"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
