8c5b61e9307ae9b5efa0d0c454227a89e3798cf9
Syed Zawad <syedzawadd@gmail.com>
2022-10-26 22:44:36 +0600

    header added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../layouts/Main';
+import Courses from '../Pages/Courses/Courses';
 
-const Routes = () => {
-    return (
-        <div>
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/courses',
+                element: <Courses></Courses>,
+                loader: () => fetch('')
+            },
+            {
+                path: '/courses/:id',
+                element: <Courses></Courses>
+            },
+        ]
 
-        </div>
-    );
-};
-
-export default Routes;
+    }
+])
