f3e44fd515e0b43a6b6479748c5c2dd209bf82e6
Syed Zawad <syedzawadd@gmail.com>
2022-10-28 00:32:39 +0600

    useauth state changed implemented

--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -1,5 +1,6 @@
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import app from '../firebase/firebase.init';
 
@@ -7,15 +8,26 @@ import app from '../firebase/firebase.init';
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-    const user = { displayName: 'Syed Zawad' }
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
 
 
 
 
     const providerSignInGoogle = provider => {
+        setLoading(true);
         return signInWithPopup(auth, provider);
     }
-
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentuser => {
+            setUser(currentuser);
+            setLoading(false);
+            console.log('observing current user', currentuser)
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
     const authInfo = { user, providerSignInGoogle }
 
