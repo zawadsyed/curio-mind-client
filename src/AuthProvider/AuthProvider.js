8228a3b19405e3ced354a1590911d4f992599423
Syed Zawad <syedzawadd@gmail.com>
2022-10-27 21:18:18 +0600

    header properly created

--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { createContext } from 'react';
 import app from '../firebase/firebase.init';
 import { getAuth } from 'firebase/auth'
+import { useState } from 'react';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const user = { displayName: 'zawad' }
+    const [user, setUser] = useState(null);
     const authInfo = { user }
 
     return (
